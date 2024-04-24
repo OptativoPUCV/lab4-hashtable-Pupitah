4312064e0c26965c85f8b01df8118fb53f873960
Pupitah <116314133+Pupitah@users.noreply.github.com>
2024-04-24 21:56:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-
+  
 
 }
 
@@ -52,8 +52,16 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-
-    return NULL;
+  HashMap * map = (HashMap *)malloc(sizeof(HashMap));
+  map->buckets = malloc(sizeof(Pair)*capacity);
+  for (int i = 0; i < capacity; i++){
+    map->buckets[i] = NULL;
+  }
+  map->size = 0;
+  map->capacity = capacity;
+  map->current = -1;
+  
+  return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
